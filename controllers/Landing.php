764da5003c5a82953563f503fad024d678960966
ProfeAlbeiro <profealbeiro2020@gmail.com>
2023-07-19 21:02:26 -0500

    Integrar las vista para el controlador Landing - html, css, js

--- controllers/Landing.php
+++ controllers/Landing.php
@@ -1,10 +1,16 @@
 <?php
     class Landing{        
         public function __construct(){}
-        public function main(){            
-            // require_once "views/company/header.view.php";
+        public function main(){
+            require_once "views/company/header.view.php";
             require_once "views/company/index.view.php";
-            // require_once "views/company/footer.view.php";
+            require_once "views/company/footer.view.php";            
+        }
+        public function about(){
+            require_once "views/company/header.view.php";
+            echo "debería ir la página";
+            // require_once "views/company/index.view.php";
+            require_once "views/company/footer.view.php";            
         }
     }
 ?>
