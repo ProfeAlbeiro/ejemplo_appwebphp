9e00a7ab36899d0e7f004fe62dba617780061aa8
ProfeAlbeiro <profealbeiro2020@gmail.com>
2023-07-19 19:57:01 -0500

    Integrar la primera vista de la app

--- controllers/Landing.php
+++ controllers/Landing.php
@@ -1,10 +1,9 @@
 <?php
     class Landing{        
         public function __construct(){}
-        public function main(){
-            echo "Estoy en el Controlador Landing y función Principal";
+        public function main(){            
             // require_once "views/company/header.view.php";
-            // require_once "views/company/index.view.php";
+            require_once "views/company/index.view.php";
             // require_once "views/company/footer.view.php";
         }
     }
---
+++ views/company/index.view.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Empresa</title>
+    <link rel="stylesheet" href="assets/css/styles_landing.css">    
+</head>
+<body>
+    <div class="container">
+        <nav class="borde">Navegador</nav>
+        <main class="borde">Área de Trabajo</main>
+        <footer class="borde">Pie de Página</footer>
+    </div>
+    <script src="assets/js/scripts_landing.js"></script>
+</body>
+</html>
