8d1b5ad6923213e127298a766c7c79338a3385f9
ProfeAlbeiro <profealbeiro2020@gmail.com>
2023-07-19 19:44:06 -0500

    Crear los archivos de arranque

---
+++ controllers/Landing.php
@@ -0,0 +1,11 @@
+<?php
+    class Landing{        
+        public function __construct(){}
+        public function main(){
+            echo "Estoy en el Controlador Landing y función Principal";
+            // require_once "views/company/header.view.php";
+            // require_once "views/company/index.view.php";
+            // require_once "views/company/footer.view.php";
+        }
+    }
+?>
