a6d893e82fbc2f38cde1add844799099ee63c0d9
ProfeAlbeiro <profealbeiro2020@gmail.com>
2023-08-30 20:41:11 -0500

    Despliegue

--- models/DataBase.php
+++ models/DataBase.php
@@ -1,14 +1,29 @@
 <?php
     class DataBase{
         public static function connection(){            
-            $hostname = "localhost";
-            $port = "3307";
+            $hostname = "dbappcompras.mysql.database.azure.com";
+            $port = "3306";
             $database = "bbdd_app_web_compras_linea";
-            $username = "root";
-            $password = "";
-			$pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8",$username,$password);
+            $username = "admin_dbappcompras";
+            $password = "Abc123.@";
+            $options = array(
+                PDO::MYSQL_ATTR_SSL_CA => 'assets/db/DigiCertGlobalRootCA.crt.pem'
+            );
+			$pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8",$username,$password,$options);
 			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			return $pdo;
 		}
 	}
+    // class DataBase{
+    //     public static function connection(){            
+    //         $hostname = "localhost";
+    //         $port = "3306";
+    //         $database = "bbdd_app_web_compras_linea";
+    //         $username = "root";
+    //         $password = "";
+	// 		$pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8",$username,$password);
+	// 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	// 		return $pdo;
+	// 	}
+	// }
 ?>
